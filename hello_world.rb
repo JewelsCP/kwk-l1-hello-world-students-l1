8be3c22f8f8f46e78ab00fcdc4a4a4b0d9b39382
JewelsCP <giulianacarballoparker@gmail.com>
2018-06-04 19:31:53 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -2,11 +2,11 @@
 puts "Hello World!"
 puts "Hello World!"
 
-print 'Hello World'
-print 'Hello World'
+print "Hello World"
+print "Hello World"
 
 
 puts "Was Braken"
 puts "Was Braken"
 
-print 'coolio man-bro'
+print "coolio man-bro"
